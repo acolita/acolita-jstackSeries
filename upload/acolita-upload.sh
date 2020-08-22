54222b5c93a2977781054393e90745339a9ac03d
raul almeida @ acolita <raul.almeida@acolita.com.br>
2020-08-22 15:41:54 -0300

    adicionado script para upload automatico de insumo para analise

---
+++ upload/acolita-upload.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+#
+# este script possibilita o upload de coletas de performance
+# para analise pelos engenheiros da Acolita [https://acolita.com.br]
+# caso necessite de ajuda para utiliza-lo, entre em contato conosco
+#
+
+if [ "$#" -lt 2 ]; then
+	me=`basename "$0"`
+	echo ""
+    echo "usage: ${me} <filename> <company> <application:optional>"
+	echo ""
+	exit
+fi
+
+which curl
+success=${?}
+
+if [ ${success} -ne 0 ]; then
+	apk add curl
+fi
+
+COMPANY=${2}
+
+fullpath=`readlink -f ${1}`
+filename=`basename ${fullpath}`
+
+APPLICATION=${3:-Default}
+
+URL="https://api.acolita.com.br/v1/file-upload"
+
+BODY='{"company": "'${COMPANY}'", "application": "'${APPLICATION}'", "file": "'${filename}'"}'
+
+api_path=`curl "${URL}" -d "${BODY}"`
+success=${?}
+
+if [ ${success} -eq 0 ]; then
+	##
+	# remove double quotes from begin and end of variable
+	api_path="${api_path%\"}"
+	api_path="${api_path#\"}"
+	##
+	curl ${api_path} --upload-file ${fullpath}
+	success=${?}
+	if [ ${success} -eq 0 ]; then
+		echo 'upload complete'
+	else
+		echo 'upload fail'
+	fi
+else
+	echo 'fail to get api_path'
+fi
