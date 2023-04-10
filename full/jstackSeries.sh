e26f9d8bce75264067474d925957d4b5cd998199
yago.leme <yago.leme@valemobi.com.br>
2023-04-10 16:05:54 -0300

    adding loading bar to improve ux

--- full/jstackSeries.sh
+++ full/jstackSeries.sh
@@ -17,6 +17,8 @@ pid=$1           # required
 count=${2:-300}  # defaults to 300 times
 delay=${3:-1}    # defaults to 1 second
 
+total=$count
+
 basic_info_file=basic_info.$(date +%s.%N).txt
 
 echo "kernel" >> ${basic_info_file}
@@ -41,7 +43,9 @@ do
 	top -H -b -n1 -p $pid > top.$pid.$(date +%s.%N)
 	sleep $delay
 	let count--
-	echo -n "."
+	
+	percentage=$(((100*(total-count))/total))
+    echo -en "\r$(printf "%*s" "$percentage" '' | tr ' ' '#') $percentage%"
 done
 
 zip jstack-collect-$(date +%s.%N).zip jstack.$pid.* top.$pid.* basic_info.*
