da38b1616ec6e8b76e1965cfab1a02c2cc5faae2
gabriel.acolita <gabriel.santos@acolita.com.br>
2023-08-09 14:39:55 -0300

    corrections on convertJsonToCollapsed.py

--- angular/conversor json-flamegraph/convertJsonToCollapsed.py
+++ angular/conversor json-flamegraph/convertJsonToCollapsed.py
@@ -1,8 +1,6 @@
 import json, sys
 
 def convert(file_name):
-    #file_name = parse_input()
-
     with open(file_name) as f:
         data = json.load(f)
         stack = "root"
@@ -15,7 +13,6 @@ def save(file_name, traces):
     new_file_name = file_name + ".txt"
     with open (new_file_name, 'w') as f:
        for line in traces:
-           #print(line)
            f.write(line + "\n")
 
 def walk_tree(data, stack, traces):
@@ -30,17 +27,13 @@ def walk_tree(data, stack, traces):
             sourceName = element["source"]
             newStack = stack + ";" + sourceName
             traces.append(newStack)
-
-        #if "duration" in element:
-        #    duration = element["duration"]
         
         if (not "children" in element["directives"][0]):
                 newStack = stack + ";" + element["directives"][0]["name"]
                 if "changeDetection" in element["directives"][0]:
                     changeDetection = element["directives"][0]["changeDetection"]
                     if (changeDetection > 0):
-                        if (len(element["children"]) == 0):
-                            newStack += ";changeDetection " + str(changeDetection)
+                        newStack += ";changeDetection " + str(changeDetection)
                 else:
                     if "lifecycle" in  element["directives"][0]:
                         if "ngDoCheck" in element["directives"][0]["lifecycle"]:
@@ -60,7 +53,6 @@ def walk_tree(data, stack, traces):
 def parse_input():
     if len(sys.argv) != 2:
         sys.exit('Example usage: python convertJsonTOCollapsed.py facebookBizTree.json')
-
     return sys.argv[1]
 
 if __name__ == "__main__":
--- angular/conversor json-flamegraph/counter.py
+++ angular/conversor json-flamegraph/counter.py
@@ -1,10 +1,10 @@
 from collections import defaultdict
 
 def process_and_sum_lines(input_file, output_file):
-    # Inicializa um dicionário para armazenar os valores somados para as linhas
+    # Inicializa um dicionario para armazenar os valores somados para as linhas
     line_sums = defaultdict(float)
     
-    # Lê o arquivo de entrada, processa e soma as linhas
+    # Le o arquivo de entrada, processa e soma as linhas
     with open(input_file, 'r') as f:
         for line in f:
             line = line.strip() # Remove a quebra de linha no final
@@ -12,25 +12,25 @@ def process_and_sum_lines(input_file, output_file):
             # Divide a linha em partes
             parts = line.split(';')
             
-            # Verifica se a última parte é um número (valor a ser somado)
+            # Verifica se a ultima parte eh um numero (valor a ser somado)
             if any(c.isdigit() for c in parts[-1]):
                 value = float(parts.pop().split(' ')[1])
             else:
                 value = 0
             
-            # Filtra os segmentos "changeDetection: <número>"
+            # Filtra os segmentos "changeDetection: <numero>"
             processed_parts = [part for part in parts if not 'changeDetection' in part]
             
             # Cria a linha processada
             processed_line = ';'.join(processed_parts)
             
-            # Adiciona o valor à soma para essa linha
+            # Adiciona o valor a soma para essa linha
             line_sums[processed_line] += value
 
-    # Escreve as linhas processadas e seus valores somados no arquivo de saída
+    # Escreve as linhas processadas e seus valores somados no arquivo de saida
     with open(output_file, 'w') as f:
         for line, summed_value in line_sums.items():
             f.write(f'{line} {summed_value:.6f}\n')
 
-# Exemplo de uso (para fins de demonstração):
-process_and_sum_lines('file_1.json.txt', 'output.txt')
+# Exemplo de uso:
+process_and_sum_lines('input.json.txt', 'output.txt')
