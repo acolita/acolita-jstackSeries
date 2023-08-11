9458bbf91ee07977ba938a39bf2f3fd316e7188f
gabriel.acolita <gabriel.santos@acolita.com.br>
2023-08-11 14:24:48 -0300

    Merging json to collapsed into a single script

--- angular/conversor json-flamegraph/convertJsonToCollapsed.py
+++ angular/conversor json-flamegraph/convertJsonToCollapsed.py
@@ -1,4 +1,5 @@
 import json, sys
+from collections import defaultdict
 
 def convert(file_name):
     with open(file_name) as f:
@@ -55,7 +56,40 @@ def parse_input():
         sys.exit('Example usage: python convertJsonTOCollapsed.py facebookBizTree.json')
     return sys.argv[1]
 
+def process_and_sum_lines(input_file, output_file):
+    # Inicializa um dicionario para armazenar os valores somados para as linhas
+    line_sums = defaultdict(float)
+    
+    # Le o arquivo de entrada, processa e soma as linhas
+    with open(input_file, 'r') as f:
+        for line in f:
+            line = line.strip() # Remove a quebra de linha no final
+            
+            # Divide a linha em partes
+            parts = line.split(';')
+            
+            # Verifica se a ultima parte eh um numero (valor a ser somado)
+            if any(c.isdigit() for c in parts[-1]):
+                value = float(parts.pop().split(' ')[1])
+            else:
+                value = 0
+            
+            # Filtra os segmentos "changeDetection: <numero>"
+            processed_parts = [part for part in parts if not 'changeDetection' in part]
+            
+            # Cria a linha processada
+            processed_line = ';'.join(processed_parts)
+            
+            # Adiciona o valor a soma para essa linha
+            line_sums[processed_line] += value
+
+    # Escreve as linhas processadas e seus valores somados no arquivo de saida
+    with open(output_file, 'w') as f:
+        for line, summed_value in line_sums.items():
+            f.write(f'{line} {summed_value:.6f}\n')
+
 if __name__ == "__main__":
     file_name = parse_input()
     traces = convert(file_name)
     save(file_name, traces)
+    process_and_sum_lines(file_name + '.txt', 'output.txt')
